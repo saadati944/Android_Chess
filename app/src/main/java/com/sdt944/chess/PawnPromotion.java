f7e84c004240660c00aaf2fa88ef3a6febdb500c
Ali Saadati <saadati944@gmail.com>
2021-03-11 11:21:40 +0330

    add pawnpromotion activity to select new chessman type

---
+++ app/src/main/java/com/sdt944/chess/PawnPromotion.java
@@ -0,0 +1,39 @@
+package com.sdt944.chess;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+public class PawnPromotion extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pawn_promotion);
+    }
+
+    public void selectNewType(View view)
+    {
+        if(view.getId() == R.id.btnQueen)
+            select(Chessman.chessmanType.Queen);
+        else if(view.getId() == R.id.btnRook)
+            select(Chessman.chessmanType.Rook);
+        else if(view.getId() == R.id.btnBishop)
+            select(Chessman.chessmanType.Bishop);
+        else if(view.getId() == R.id.btnKnight)
+            select(Chessman.chessmanType.Knight);
+        else
+            select(Chessman.chessmanType.Pawn);
+    }
+    void select(Chessman.chessmanType newType)
+    {
+        PromotionResault.result = newType;
+        Intent returnIntent = new Intent();
+        setResult(Activity.RESULT_CANCELED, returnIntent);
+        finish();
+    }
+}
