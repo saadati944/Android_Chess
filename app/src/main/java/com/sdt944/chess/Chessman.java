cd5f49f847450687520bb2e5182de49da6630e7c
Ali Saadati <saadati944@gmail.com>
2021-03-11 18:07:03 +0330

    add all possible moves to chessman class

--- app/src/main/java/com/sdt944/chess/Chessman.java
+++ app/src/main/java/com/sdt944/chess/Chessman.java
@@ -11,10 +11,25 @@ public abstract class Chessman {
         Knight,
         Pawn
     }
+    /*
+     *  .BLACK..
+     *  ........
+     *  ........
+     *  ...X....
+     *  ........
+     *  ........
+     *  ........
+     *  .WHITE..
+     * */
+    public enum playerColor {
+        Black,
+        White
+    }
 
     private Point point;
     public ArrayList<Point> moves;
     public chessmanType type;
+    public playerColor color;
 
     public ArrayList<Point> getMoves()
     {
@@ -27,5 +42,220 @@ public abstract class Chessman {
     public Point getPoint()
     {
         return point;
+    }
+
+    public void addVerticalMovePoints() {
+        /*
+         *  ...#....
+         *  ...#....
+         *  ...#....
+         *  ...X....
+         *  ...#....
+         *  ...#....
+         *  ...#....
+         *  ...#....
+         * */
+        for(int j = 0; j <8; j++)
+        {
+            Point p = new Point(point.x, j);
+
+            if(j == point.y || moves.contains(p))
+                continue;
+
+            moves.add(p);
+        }
+    }
+    public void addHorizontalMovePoints() {
+        /*
+         *  ........
+         *  ........
+         *  ........
+         *  ###X####
+         *  ........
+         *  ........
+         *  ........
+         *  ........
+         * */
+        for(int i=0; i<8; i++)
+        {
+            Point p = new Point(i, point.y);
+
+            if(i == point.x || moves.contains(p))
+                continue;
+
+            moves.add(p);
+        }
+    }
+    public void addAroundMovePoints() {
+        /*
+         *  ........
+         *  ........
+         *  ..###...
+         *  ..#X#...
+         *  ..###...
+         *  ........
+         *  ........
+         *  ........
+         * */
+        for(int i=this.getPoint().x-1; i<this.getPoint().x+2; i++)
+            for(int j=this.getPoint().y-1; j<this.getPoint().y+2; j++)
+                if(Point.isValid(i, j) && point.x != i && point.y != j)
+                    this.moves.add(new Point(i, j));
+    }
+    public void addObliqueNWtoSEMovePoints() {
+        /*
+         *  #.......
+         *  .#......
+         *  ..#.....
+         *  ...X....
+         *  ........
+         *  ........
+         *  ........
+         *  ........
+         * */
+        int i = point.x-1, j = point.y-1;
+        while(Point.isValid(i, j))
+        {
+            Point p = new Point(i, j);
+
+            if(!moves.contains(p))
+            {
+                moves.add(p);
+            }
+
+            i--;
+            j--;
+        }
+        /*
+         *  ........
+         *  ........
+         *  ........
+         *  ...X....
+         *  ....#...
+         *  .....#..
+         *  ......#.
+         *  .......#
+         * */
+        i = point.x+1;
+        j = point.y+1;
+        while(Point.isValid(i, j))
+        {
+            Point p = new Point(i, j);
+
+            if(!moves.contains(p))
+            {
+                moves.add(p);
+            }
+
+            i++;
+            j++;
+        }
+    }
+    public void addObliqueNEtoSWMovePoints() {
+        /*
+         *  ......#.
+         *  .....#..
+         *  ....#...
+         *  ...X....
+         *  ........
+         *  ........
+         *  ........
+         *  ........
+         * */
+        int i = point.x+1, j = point.y-1;
+        while(Point.isValid(i, j))
+        {
+            Point p = new Point(i, j);
+
+            if(!moves.contains(p))
+            {
+                moves.add(p);
+            }
+
+            i++;
+            j--;
+        }
+        /*
+         *  ........
+         *  ........
+         *  ........
+         *  ...X....
+         *  ..#.....
+         *  .#......
+         *  #.......
+         *  ........
+         * */
+        i = point.x-1;
+        j = point.y+1;
+        while(Point.isValid(i, j))
+        {
+            Point p = new Point(i, j);
+
+            if(!moves.contains(p))
+            {
+                moves.add(p);
+            }
+
+            i--;
+            j++;
+        }
+    }
+    public void add1StepForwardMovePoints() {
+        if(color == playerColor.Black)
+        {
+            addForwardMovePoints(1);
+            return;
+        }
+        addForwardMovePoints(-1);
+    }
+    public void add2StepForwardMovePoints() {
+        if(color == playerColor.Black)
+        {
+            addForwardMovePoints(2);
+            return;
+        }
+        addForwardMovePoints(-2);
+    }
+    private void addForwardMovePoints(int step) {
+        Point p = new Point(point.x+step, point.y);
+        if(p.isValid() && !moves.contains(p))
+            moves.add(p);
+    }
+    public void addLMovePoints() {
+        /*
+         *  ........
+         *  ..#.#...
+         *  .#...#..
+         *  ...X....
+         *  .#...#..
+         *  ..#.#...
+         *  ........
+         *  ........
+         * */
+        Point p = new Point(point.x - 1, point.y - 2);
+        if (p.isValid() && !moves.contains(p))
+            moves.add(p);
+        p = new Point(point.x - 1, point.y + 2);
+        if (p.isValid() && !moves.contains(p))
+            moves.add(p);
+        p = new Point(point.x - 2, point.y - 1);
+        if (p.isValid() && !moves.contains(p))
+            moves.add(p);
+        p = new Point(point.x - 2, point.y + 1);
+        if (p.isValid() && !moves.contains(p))
+            moves.add(p);
+
+        p = new Point(point.x + 1, point.y - 2);
+        if (p.isValid() && !moves.contains(p))
+            moves.add(p);
+        p = new Point(point.x + 1, point.y + 2);
+        if (p.isValid() && !moves.contains(p))
+            moves.add(p);
+        p = new Point(point.x + 2, point.y - 1);
+        if (p.isValid() && !moves.contains(p))
+            moves.add(p);
+        p = new Point(point.x + 2, point.y + 1);
+        if (p.isValid() && !moves.contains(p))
+            moves.add(p);
     }
 }
