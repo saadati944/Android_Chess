adfb9b2be0cf92e00ca60be531162403b86cca87
Ali Saadati <saadati944@gmail.com>
2021-03-11 11:19:58 +0330

    add PromotionResault class

---
+++ app/src/main/java/com/sdt944/chess/PromotionResault.java
@@ -0,0 +1,11 @@
+package com.sdt944.chess;
+
+
+/*
+* Why I didn't use startActivityForResult() ???
+* Because this one is easier ...
+* */
+
+public class PromotionResault {
+    public static Chessman.chessmanType result = Chessman.chessmanType.Pawn;
+}
