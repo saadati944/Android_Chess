5d9c8d1276496cc553f4aedebae4a6f3242d0009
Ali Saadati <saadati944@gmail.com>
2021-03-13 20:08:37 +0330

    create board cells programmaticaly and save them to a 2d array

--- app/src/main/java/com/sdt944/chess/ChessBoard.java
+++ app/src/main/java/com/sdt944/chess/ChessBoard.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.view.ViewManager;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TableRow;
@@ -13,22 +14,20 @@ import android.widget.Toast;
 
 public class ChessBoard extends AppCompatActivity {
 
+    public View sampleCell;
+    public View[][] board=new View[8][8];
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chess_board);
+        sampleCell = findViewById(R.id.sampleCell);
+
+        createCells();
     }
-    public void btnTmpClick(View view)
-    {
+    public void btnTmpClick(View view) {
         startActivityForResult(new Intent(this, PawnPromotion.class), 0);
         TableRow row1 = findViewById(R.id.boardRow1);
-
-        Button btn = new Button(new ContextThemeWrapper(this, R.style.Widget_AppCompat_Button_Small), null, 0);
-        btn.setLayoutParams(view.getLayoutParams());
-
-        row1.addView(btn);
-
-        Toast.makeText(this, "added", Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -36,5 +35,32 @@ public class ChessBoard extends AppCompatActivity {
 
         Toast.makeText(this, PromotionResault.result.toString(), Toast.LENGTH_SHORT).show();
 
+    }
+    void createCells() {
+        TableRow[] rows = new TableRow[8];
+        rows[0] = findViewById(R.id.boardRow1);
+        rows[1] = findViewById(R.id.boardRow2);
+        rows[2] = findViewById(R.id.boardRow3);
+        rows[3] = findViewById(R.id.boardRow4);
+        rows[4] = findViewById(R.id.boardRow5);
+        rows[5] = findViewById(R.id.boardRow6);
+        rows[6] = findViewById(R.id.boardRow7);
+        rows[7] = findViewById(R.id.boardRow8);
+
+        for(int i=0;i<8;i++)
+            for(int j=0;j<8;j++)
+                rows[j].addView(createNewCell());
+
+        removeSampleCell();
+    }
+    View createNewCell() {
+        //todo : change button with a better view like imageView.
+        Button cell = new Button(new ContextThemeWrapper(this, R.style.Widget_AppCompat_Button_Small), null, 0);
+        cell.setLayoutParams(sampleCell.getLayoutParams());
+
+        return cell;
+    }
+    void removeSampleCell() {
+        ((ViewManager)sampleCell.getParent()).removeView(sampleCell);
     }
 }
