bc86a9d7d3ed80ee420011de89459507271d4e3c
Ali Saadati <saadati944@gmail.com>
2021-03-13 19:52:26 +0330

    change linear layout with table in chessboard

--- app/src/main/java/com/sdt944/chess/ChessBoard.java
+++ app/src/main/java/com/sdt944/chess/ChessBoard.java
@@ -4,7 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextThemeWrapper;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TableRow;
 import android.widget.Toast;
 
 public class ChessBoard extends AppCompatActivity {
@@ -17,6 +21,14 @@ public class ChessBoard extends AppCompatActivity {
     public void btnTmpClick(View view)
     {
         startActivityForResult(new Intent(this, PawnPromotion.class), 0);
+        TableRow row1 = findViewById(R.id.boardRow1);
+
+        Button btn = new Button(new ContextThemeWrapper(this, R.style.Widget_AppCompat_Button_Small), null, 0);
+        btn.setLayoutParams(view.getLayoutParams());
+
+        row1.addView(btn);
+
+        Toast.makeText(this, "added", Toast.LENGTH_SHORT).show();
     }
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
